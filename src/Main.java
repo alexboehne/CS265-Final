c717534f054ba0834d3b81d1dd1e0d4a17afd722
alexboehne <alex.boehne04@gmail.com>
2024-04-11 11:08:20 -0500

    -Whoops I forgot to make the program recursive lol ---Removed charArray breakdown and instead pass whole string to calcPalindrome
    -Changed String concat to StringBuilder for speed

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
-        ArrayList<String> argList = new ArrayList<String>(Arrays.asList(args)); // Cast args to arraylist
+        ArrayList<String> argList = new ArrayList<>(Arrays.asList(args)); // Cast args to arraylist
 
         Scanner input = new Scanner(System.in); // Scanner obj
 
@@ -20,7 +20,7 @@ public class Main {
         comparePassword(compedPass, input); // Doesn't advance program until correct password
 
         // Init FileReader
-        FileReader f = null;
+        FileReader f;
 
         // Change the text file directory if used on different machine (specified on CLI with arg -p <PATH>)
         if (argList.contains("-p")) { // Checks for -p and sets filepath to following arg
@@ -45,7 +45,6 @@ public class Main {
 
         String[] stList = splitFile(f); // Puts text file in string array
 
-        ArrayList<Character> charArray = new ArrayList<>();
         ArrayList<String> palindromeArray = new ArrayList<>();
 
         // Iterates through all words in stored list
@@ -57,14 +56,10 @@ public class Main {
                     continue;
                 }
             }
-            for (Character c : word.toCharArray()) { // Turn word into character array
-                charArray.add(c);
-            }
-            if (calcPalindrome(charArray)) { // Adds to palindrome list
+            if (calcPalindrome(word)) { // Adds to palindrome list
                 palindromeArray.add(word);
                 palindromes++;
             }
-            charArray.clear(); // Clears charArray
         }
 
         long end = System.currentTimeMillis();
@@ -72,7 +67,7 @@ public class Main {
 
         System.out.println(palindromes + " palindromes found in " + time + " ms"); // Print output
 
-        // List all pallindromes found
+        // List all palindromes found
         System.out.println("Would you like to list the palindromes found? (y/N)");
         String response = input.next();
         if (response.equals("y")) {
@@ -83,18 +78,13 @@ public class Main {
         }
     }
 
-    // Function to determine if word is pallindrome
-    public static boolean calcPalindrome(ArrayList<Character> t) {
-        for (int i = 0; i <= Math.ceil(t.size() / 2); i++) {
-            // Converts characters to lowercase
-            Character c = Character.toLowerCase(t.get(i));
-            Character c2 = Character.toLowerCase(t.get(t.size() - 1 - i));
-
-            if (!c.equals(c2)) { // Checks for 2 letters at opposite ends
-                return false;
-            }
+    // Function to determine if word is palindrome
+    public static boolean calcPalindrome(String word) {
+        if (word.charAt(0) != word.charAt(word.length() - 1)) { // If first and last characters don't match, not a palindrome
+            return false;
+        } else { // Recursively check the substring excluding first and last characters
+            return calcPalindrome(word.substring(1, word.length() - 1));
         }
-        return true;
     }
 
     //  Digests input string into SHA-256 hex byte array
@@ -134,10 +124,10 @@ public class Main {
 
     public static String[] splitFile(FileReader fr) throws IOException {
         // list that holds strings of a file
-        ArrayList<String> listOfStrings = new ArrayList<String>();
+        ArrayList<String> listOfStrings = new ArrayList<>();
 
 
-        String s = "";
+        StringBuilder s = new StringBuilder();
         char ch;
 
         // Reads file
@@ -148,18 +138,18 @@ public class Main {
             if (ch == '\n' || ch == ' ' || ch == ',') {
 
                 // Storing each string in arraylist
-                listOfStrings.add(s);
+                listOfStrings.add(s.toString());
 
                 // clearing content in string
-                s = "";
+                s = new StringBuilder();
             } else {
-                s += ch;
+                s.append(ch);
             }
         }
-        if (!s.isEmpty()) {
+        if (s.length() > 0) {
 
             // Appends last line of strings
-            listOfStrings.add(s);
+            listOfStrings.add(s.toString());
         }
         // storing the data in arraylist to array (faster read times)
         return listOfStrings.toArray(new String[0]);
